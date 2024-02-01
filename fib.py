8a7a696284a3fd8a4e7f232220ab2fc9e1a9941a
cstomp <98862811+cstomp@users.noreply.github.com>
2024-02-01 12:39:35 -0600

    Update fib.py
    Adding Numbers

--- fib.py
+++ fib.py
@@ -7,10 +7,11 @@ import matplotlib.pyplot as pyplot
 
 def timer(func): 
     def wrap_func(*args, **kwargs): 
+        num = args
         t1 = time() 
         result = func(*args, **kwargs) 
         t2 = time() 
-        print(f'Finished in {(t2-t1):.4f}s') 
+        print(f'Finished in {(t2-t1):.4f}s : f{(num)}') 
         return result 
     return wrap_func 
   
